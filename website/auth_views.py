657a96e44be5886ccb198038f5c24981f3d5c346
Vr3n <vickyspatel@gmail.com>
2022-09-03 23:46:07 +0530

    templates created

--- website/auth_views.py
+++ website/auth_views.py
@@ -1,5 +1,20 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Think of this as Urls.
 auth_views = Blueprint('auth_views', __name__)
+
+
+@auth_views.route("login/")
+def login():
+    return render_template("login.html")
+
+
+@auth_views.route("logout/")
+def logout():
+    return "<p>logout</p>"
+
+
+@auth_views.route("sign-up/")
+def sign_up():
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Think of this as Urls.
 views = Blueprint('views', __name__)
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Hello, Flask!</h1>"
+    return render_template("home.html")
